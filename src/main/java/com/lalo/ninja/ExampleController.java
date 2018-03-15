b8391447e405d37f71029e24ced5f1a893640556
Eduardo Ocampo <edd.develit@gmail.com>
2018-03-15 13:40:19 -0600

    Insertando datos en plantillas

--- src/main/java/com/lalo/ninja/ExampleController.java
+++ src/main/java/com/lalo/ninja/ExampleController.java
@@ -1,6 +1,7 @@
 package com.lalo.ninja;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,14 +14,18 @@ public class ExampleController {
 
 	// Primera forma
 	@RequestMapping(value = "exampleString", method = RequestMethod.GET)
-	public String exampleString() {
+	public String exampleString(Model model) {
+		model.addAttribute("name", "Perro");
 		return EXAMPLE_VIEW;
 	}
 
 	// Segunda forma
 	@RequestMapping(value = "exampleMAV", method = RequestMethod.GET)
 	public ModelAndView exampleMAV() {
-		return new ModelAndView(EXAMPLE_VIEW);
+		
+		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+		mav.addObject("name", "Perros");
+		return mav;
 	}
 
 }
